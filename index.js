558b34e98cf634c5a84fda5eb113afe08cde8ef5
Saim Rahdari <43989990+saimrahdari@users.noreply.github.com>
2023-06-10 16:09:06 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const { TestCase_2 } = require('./TestCase_2'); const { TestCase_3 } = require('./TestCase_3'); const { TestCase_4 } = require('./TestCase_4'); const { TestCase_5 } = require('./TestCase_5'); 
+TestCase_1();
+TestCase_2();
+TestCase_3();
+TestCase_4();
+TestCase_5();
